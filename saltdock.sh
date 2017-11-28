385ebf898759495e44eae89812960e2c16ff441a
Daniel Coman <daniel.alex.coman@gmail.com>
2017-11-28 16:01:18 +0200

    Fixed namespace bugs in saltdock cli

--- saltdock.sh
+++ saltdock.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 
 # Swarm is incompatible with features needed for a systemd image
-# saltdock.sh imitates swarm format and adds systemd deb image to swarm network
+# saltdock.sh mimics swarm format and adds systemd deb image to swarm network
 
 # Swarm network
-network="saltdock_internal"
 namespace="saltdock"
-
-# Image, fallback to deb
-if [[ $3 -eq $3 ]] ; then
-  img_type=$3
-else
-  img_type="deb"
-fi
-
-img="saltdock_img_${img_type}_systemd"
+network="${namespace}_internal"
 
 # Naming and scope
 name="cluster"
 SALT_TYPE="minion"
 
+# Image, fallback to deb
+if [ -z "$3" ] ; then
+  img_type="deb"
+else
+  img_type="$3"
+fi
+img="${namespace}_img_${img_type}_systemd"
+
+
 # Check network prerequisite
 swarm_check () {
 docker network ls | grep ${network}
-[ $? -ne 0 ] && echo "Error. First build saltdock env first." && exit 1
+[ $? -ne 0 ] && echo "Error. First build ${namespace} env first." && exit 1
 }
 
 # Reproduce docker swarm naming scheme
 swarm_name () {
-  increment_list=$(docker ps -a --format "{{.Names}}" | grep "${name}_${img_type}" | awk -F'.' '{print $2}')
+  increment_list=$(docker ps -a --format "{{.Names}}" | grep "${namespace}_${name}_${img_type}" | awk -F'.' '{print $2}')
   increment=$(for nr in ${increment_list} ; do echo $nr ; done | sort -nr | head -1)
   [[ increment -eq increment ]] && increment=$((increment+1)) || increment=0 > /dev/null 2>&1
 }
 
 add () {
   docker run -d \
-    --name "saltdock_${name}_${img_type}.${increment}.$(openssl rand -hex 10)" \
+    --name "${namespace}_${name}_${img_type}.${increment}.$(openssl rand -hex 10)" \
     --security-opt seccomp=unconfined \
     --tmpfs /run --tmpfs /run/lock \
     --volume /sys/fs/cgroup:/sys/fs/cgroup:ro \
@@ -46,14 +46,11 @@ add () {
 }
 
 remove () {
-  docker stop `docker ps | grep ${namespace} | awk '{print $1}' `
-  docker rm `docker ps -a | grep ${namespace} | awk '{print $1}' `
+  echo "${img}"
+  docker stop `docker ps  | grep "${img}" | awk '{print $1}' `
+  docker rm `docker ps -a | grep "${img}" | awk '{print $1}' `
 }
 
-# # config
-# init () {
-# mkdir -p /var/saltdock && touch ${conf} > /dev/null 2>&1
-# }
 
 case "$1" in
   add)
